9b19c1a10d1bbf631a5fde1475c5a206122974be
sasidelnikova <109522558+sasidelnikova@users.noreply.github.com>
2022-07-19 13:16:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-# This is a sample Python script.
+class Person:
+    def __init__(self, name):
+        self.name = name
+        self.age = 1
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def display_info(self):
+        print(f"Name: {self.name}    Age: {self.age}")
 
-price = int(input())
 
-if price % 10 != 0: print(0)
-else:
-    two = price // 200
-    price -= two * 200
-    one = price // 100
-    price -= one * 100
-    five = price // 50
-    price -= five * 50
+tom = Person("tommy")
+tom.age = 18
+tom.display_info()
 
-    if price > 0: print(0)
-    else:
-        print(two * 4 + one * 2 + five)
+zhenya = Person("evg")
+zhenya.age = 16
+zhenya.display_info()
