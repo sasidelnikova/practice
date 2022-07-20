c68fa88ee173d6f26c952b50f6acb3de81fe46d5
sasidelnikova <109522558+sasidelnikova@users.noreply.github.com>
2022-07-20 14:40:52 +0300

    Update create_json_file.py

--- create_json_file.py
+++ create_json_file.py
@@ -25,4 +25,4 @@ my_json = """{
 """
 my_json_string = json.loads(my_json)
 with open('test_file.json', 'w') as file:
-    json.dump(my_json_string, file)
+    json.dump(my_json_string, file, indent=4)
