fb3a809d9ab9cb08f8db5f1563b7f117f9f3db28
sasidelnikova <109522558+sasidelnikova@users.noreply.github.com>
2022-07-20 14:02:07 +0300

    Update print_json.py

--- print_json.py
+++ print_json.py
@@ -1,5 +1,10 @@
 import json
 
+# печать всего JSON-объекта:
 with open('test_file.json', 'r') as j:
     json_data = json.load(j)
     print(json_data)
+
+# печать части JSON-объекта:
+json_str = json.loads(json_data)
+print(json_str["blog"])
