5659ad24f4a17d3c7e48bc1a133c1a7332a3f608
sasidelnikova <109522558+sasidelnikova@users.noreply.github.com>
2022-07-20 15:06:17 +0300

    Update print_json.py

--- print_json.py
+++ print_json.py
@@ -3,8 +3,8 @@ import json
 # печать всего JSON-объекта:
 with open('test_file.json', 'r') as j:
     json_data = json.load(j)
-    print(json_data)
+    json_data_str = json.dumps(json_data, indent=4)
+    print(json_data_str)
 
 # печать части JSON-объекта:
-json_str = json.loads(json_data)
-print(json_str["blog"])
+print(json_data["blog"])
