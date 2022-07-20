b13123aa52f5af21a852b7498b96d555b6cead93
sasidelnikova <109522558+sasidelnikova@users.noreply.github.com>
2022-07-20 13:25:38 +0300

    Add files via upload

---
+++ json test.py
@@ -0,0 +1,27 @@
+import json
+
+my_json_string = """{
+   "article": [
+      {
+         "id":"01",
+         "language": "JSON",
+         "edition": "first",
+         "author": "Derrick Mwiti"
+      },
+      {
+         "id":"02",
+         "language": "Python",
+         "edition": "second",
+         "author": "Derrick Mwiti"
+      }
+   ],
+   "blog":[
+   {
+       "name": "Datacamp",
+       "URL":"datacamp.com"
+   }
+   ]
+}
+"""
+with open('test_file.json', 'w') as file:
+    json.dump(my_json_string, file)
